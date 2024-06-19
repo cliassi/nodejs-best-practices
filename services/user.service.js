2a7e26e0a4d291afb6b3cadf7e0e89be50f7ba81
RDELLI\rrony <rronyy@gmail.com>
2024-06-19 16:06:46 +0800

    add user service

--- services/user.service.js
+++ services/user.service.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const {User, validate} = require('../models/user');
+
 exports.getUser = async function getUser(id){
   const user = await User.findById(id).select('-password');
     return user;
@@ -6,10 +10,10 @@ exports.createUser = async function createUser(body) {
   const { error } = validate(body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let user = await User.findOne({ email: req.body.email });
+  let user = await User.findOne({ email: body.email });
   if (user) return res.status(400).send("User already registered.");
 
-  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  user = new User(_.pick(body, ["name", "email", "password"]));
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
